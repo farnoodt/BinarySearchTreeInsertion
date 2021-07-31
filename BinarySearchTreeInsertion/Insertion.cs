f5b1ec8ebc14f88567390d2e6dc392faabc0167a
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-07-31 00:15:54 -0700

    Add project files.

---
+++ BinarySearchTreeInsertion/Insertion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeInsertion
+{
+    class Insertion
+    {
+        public Node Insert(Node root, int data)
+        {
+            if (root == null)
+                return new Node(data);
+
+            if (data <= root.data)
+                root.left = Insert(root.left, data);
+            else
+                root.right = Insert(root.right, data);
+
+            return root;
+        }
+    }
+}
---
+++ BinarySearchTreeInsertion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BinarySearchTreeInsertion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Insertion In = new Insertion();
+            Node tree = new Node(10);
+            In.Insert(tree, -5);
+            In.Insert(tree, -8);
+            In.Insert(tree, 7);
+            In.Insert(tree, 16);
+            In.Insert(tree, 18);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
